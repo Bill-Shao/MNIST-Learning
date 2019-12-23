b1da8a0cf635c5455eae2fee1c99b6e6aeed04fd
BillSha <billshao17@gmail.com>
2019-12-22 21:05:18 -0800

    Added Backprop

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ images, labels = dataiter.next()
 print(images.shape)
 print(labels.shape)
 
+#showing data 
 figure = plt.figure()
 num_of_images = 60
 for index in range(1, num_of_images + 1):
@@ -25,6 +26,7 @@ for index in range(1, num_of_images + 1):
     plt.axis('off')
     plt.imshow(images[index].numpy().squeeze(), cmap='gray_r')
 plt.show()
+
 
 input_size = 784
 hidden_sizes = [128, 64]
@@ -48,3 +50,54 @@ loss = criterion(logps, labels) #calculate the NLL loss
 print('Before backward pass: \n', model[0].weight.grad)
 loss.backward()
 print('After backward pass: \n', model[0].weight.grad)
+
+optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
+time0 = time()
+epochs = 15
+for e in range(epochs):
+    running_loss = 0
+    for images, labels in trainloader:
+        # Flatten MNIST images into a 784 long vector
+        images = images.view(images.shape[0], -1)
+    
+        # Training pass
+        optimizer.zero_grad()
+        
+        output = model(images)
+        loss = criterion(output, labels)
+        
+        #This is where the model learns by backpropagating
+        loss.backward()
+        
+        #And optimizes its weights here
+        optimizer.step()
+        
+        running_loss += loss.item()
+    else:
+        print("Epoch {} - Training loss: {}".format(e, running_loss/len(trainloader)))
+print("\nTraining Time (in minutes) =",(time()-time0)/60)
+
+images, labels = next(iter(valloader))
+
+img = images[0].view(1, 784)
+with torch.no_grad():
+    logps = model(img)
+
+correct_count, all_count = 0, 0
+for images,labels in valloader:
+  for i in range(len(labels)):
+    img = images[i].view(1, 784)
+    with torch.no_grad():
+        logps = model(img)
+
+    
+    ps = torch.exp(logps)
+    probab = list(ps.numpy()[0])
+    pred_label = probab.index(max(probab))
+    true_label = labels.numpy()[i]
+    if(true_label == pred_label):
+      correct_count += 1
+    all_count += 1
+
+print("Number Of Images Tested =", all_count)
+print("\nModel Accuracy =", (correct_count/all_count))
